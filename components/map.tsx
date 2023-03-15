28c6b710fdd919908ed64806387049ab7b623c3b
Everett Lindsay <everettsjunk@gmail.com>
2023-03-15 09:48:46 -0400

    trying to restrict the API key

--- components/map.tsx
+++ components/map.tsx
@@ -14,13 +14,26 @@ type DirectionsResult = google.maps.DirectionsResult;
 type MapOptions = google.maps.MapOptions;
 
 export default function Map() {
-	const center = useMemo(() => ({ lat: 43, lng: -80 }), []);
+	const mapRef = useRef<GoogleMap>();
+	const center = useMemo<LatLngLiteral>(() => ({ lat: 43, lng: -80 }), []);
+	const options = useMemo<MapOptions>(() => ({
+		disableDefaultUI: true,
+		clickableIcons: false
+	}), []);
+	const onLoad = useCallback(map => (mapRef.current = map), []);
+
 	return <div className="container">
 		<div className="controls">
 			<h1>Commute?</h1>
 		</div>
 		<div className="map">
-			<GoogleMap zoom={10} center={center} mapContainerClassName="map-container"></GoogleMap>
+			<GoogleMap
+				zoom={10}
+				center={center}
+				mapContainerClassName="map-container"
+				options={options}
+				onLoad={onLoad}
+			></GoogleMap>
 		</div>
 	</div>;
 }
