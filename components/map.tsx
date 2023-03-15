ac0c5521e1e5e3b293b422688a2acbac0dcb893b
Everett Lindsay <everettsjunk@gmail.com>
2023-03-15 18:29:56 -0400

    blindly stabbing at a compilation solution

--- components/map.tsx
+++ components/map.tsx
@@ -86,7 +86,7 @@ export default function Map() {
 
 						<MarkerClusterer>
 							{clusterer =>
-								houses.map(house => (
+								houses?.map(house => (
 									<Marker
 										key={house.lat}
 										position={house}
